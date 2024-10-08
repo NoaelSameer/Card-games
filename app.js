56fa1409f57c9983446290008c18759fcb11f393
Oliver Kuopus <141285426+oakuopus@users.noreply.github.com>
2024-10-08 14:07:04 -0700

    turntoasigma

--- app.js
+++ app.js
@@ -5,3 +5,17 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+    res.json("index");
+})
+
+app.get("/blackjack", (req, res) => {
+    res.json("blackjack");
+})
+
+
+
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+})
